f150a99d5f543186786b995290fb3670fe5249b0
Ashwini <salunkheashwini1998@gmail.com>
2023-01-16 16:32:22 +0530

    Added UC1 First Name

--- Program.cs
+++ Program.cs
@@ -3,20 +3,25 @@ using System.Text.RegularExpressions;
 
 namespace UserRegistration
 {
-    class Program
+    internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter Email");
-            string inStr = Console.ReadLine();
-            string firstName = @"^[A-Z]{1}[a-zA-Z]{2,9}$";
-            string lastName = @"^[A-Z]{1}[a-zA-Z]{2,9}$";
-            string emailId = "^[a-zA-Z0-9]+[._+-]{0,1}[a-zA-Z0-9]+@[a-zA-Z0-9]{1,10}[.][a-zA-Z]{2,10}[.]*[a-zA-Z]*$";
-            string phoneNum = @"[0-9]{2}\s+[1-9]{10}$";
-            string password = @"[a-zA-Z0-9]{1}[A-Z][0-9][@#$%&*_-]+[a-zA-Z0-9]$";
+            Console.WriteLine("Welcome To Regex User Registration Problem");
+            Console.WriteLine("Regex User Registration First Name");
+            Console.WriteLine("Enter first Name");
 
-            Regex regex = new Regex(emailId);
-            Console.WriteLine(regex.IsMatch(inStr));
+            string text = Console.ReadLine();
+            //check the given input contain
+
+            if (Regex.Match(text, "^[A-Z][a-z]{3}$").Success)
+            {
+                Console.WriteLine("True");
+            }
+            else
+            {
+                Console.WriteLine("False");
+            }
         }
     }
 }
