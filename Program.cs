b3f5656271d89347aa23f6f2743425ef0bfc3fde
Ashwini <salunkheashwini1998@gmail.com>
2023-01-16 21:26:42 +0530

    Added Special character

--- Program.cs
+++ Program.cs
@@ -13,7 +13,7 @@ namespace UserRegistration
             string lastName = @"^[A-Z]{1}[a-zA-Z]{2,9}$";
             string emailId = @"^[a-zA-Z0-9]+[._+-]{0,1}[a-zA-Z0-9]@[a-zA-Z0-9]{1,10}.[a-zA-Z]{2,10}[.][a-zA-Z]";
             string phoneNum = @"[0-9]{2}\s+[1-9]{10}$";
-            string password = @"[A-Z]{1}[a-zA-Z]{7}$";
+            string password = @"[a-zA-Z0-9]{1}[A-Z][0-9][@#$%&*_-]+[a-zA-Z0-9]$";
 
             Regex regex = new Regex(password);
             Console.WriteLine(regex.IsMatch(inStr));
