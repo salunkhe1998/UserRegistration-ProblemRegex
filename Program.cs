78a64936f4bb2d4dc92962c23f4d7943bdd3d73d
Ashwini <salunkheashwini1998@gmail.com>
2023-01-16 21:37:31 +0530

    this is sample Email file

--- Program.cs
+++ Program.cs
@@ -7,15 +7,15 @@ namespace UserRegistration
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter Password");
+            Console.WriteLine("Enter Email");
             string inStr = Console.ReadLine();
             string firstName = @"^[A-Z]{1}[a-zA-Z]{2,9}$";
             string lastName = @"^[A-Z]{1}[a-zA-Z]{2,9}$";
-            string emailId = @"^[a-zA-Z0-9]+[._+-]{0,1}[a-zA-Z0-9]@[a-zA-Z0-9]{1,10}.[a-zA-Z]{2,10}[.][a-zA-Z]";
+            string emailId = "^[a-zA-Z0-9]+[._+-]{0,1}[a-zA-Z0-9]+@[a-zA-Z0-9]{1,10}[.][a-zA-Z]{2,10}[.]*[a-zA-Z]*$";
             string phoneNum = @"[0-9]{2}\s+[1-9]{10}$";
             string password = @"[a-zA-Z0-9]{1}[A-Z][0-9][@#$%&*_-]+[a-zA-Z0-9]$";
 
-            Regex regex = new Regex(password);
+            Regex regex = new Regex(emailId);
             Console.WriteLine(regex.IsMatch(inStr));
         }
     }
